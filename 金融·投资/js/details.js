c55c6e2468780c327bd41bd4f7fc9714851df158
TheMoodH <46800927+TheMoodH@users.noreply.github.com>
2019-04-30 14:15:25 +0800

    Add files via upload

---
+++ 金融·投资/js/details.js
@@ -0,0 +1,8 @@
+var nav = document.getElementsByClassName("nav");
+var navin = document.getElementById('navin');
+function navon(){
+	navin.style.background="transparent";
+}
+function navout(){
+	navin.style.background="#a11c33";
+}
---
+++ 金融·投资/js/product.js
@@ -0,0 +1,45 @@
+var product = document.getElementsByClassName("product_tub_rg");
+productul = product[0].getElementsByTagName("ul");
+productli = productul[0].getElementsByTagName("li");
+productli[0].onclick = function(){
+	for(var i = 0;i<productli.length;i++){
+		productli[i].setAttribute("class","");
+	}
+	productli[0].setAttribute("class","pruc");
+}
+productli[1].onclick = function(){
+	for(var i = 0;i<productli.length;i++){
+		productli[i].setAttribute("class","");
+	}
+	productli[1].setAttribute("class","pruc");
+}
+productli[2].onclick = function(){
+	for(var i = 0;i<productli.length;i++){
+		productli[i].setAttribute("class","");
+	}
+	productli[2].setAttribute("class","pruc");
+}
+productli[3].onclick = function(){
+	for(var i = 0;i<productli.length;i++){
+		productli[i].setAttribute("class","");
+	}
+	productli[3].setAttribute("class","pruc");
+}
+productli[4].onclick = function(){
+	for(var i = 0;i<productli.length;i++){
+		productli[i].setAttribute("class","");
+	}
+	productli[4].setAttribute("class","pruc");
+}
+productli[5].onclick = function(){
+	for(var i = 0;i<productli.length;i++){
+		productli[i].setAttribute("class","");
+	}
+	productli[5].setAttribute("class","pruc");
+}
+productli[6].onclick = function(){
+	for(var i = 0;i<productli.length;i++){
+		productli[i].setAttribute("class","");
+	}
+	productli[6].setAttribute("class","pruc");
+}
